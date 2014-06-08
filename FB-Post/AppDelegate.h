62226ee0f070fb8e6b346589813d3e28589e3586
Kristina Frost <kristinafrost@tw-mbp15retina-kfrost.gateway.2wire.net>
2014-06-08 09:22:26 -0700

    Initial Commit

---
+++ FB-Post/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FB-Post
+//
+//  Created by Kristina Frost on 6/8/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
